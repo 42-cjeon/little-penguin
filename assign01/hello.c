274b0f8ca58611fc0caf680211676cc333e5ef0f
cjeon <cjeon@student.42seoul.kr>
2023-01-17 22:55:54 +0000

    refactor: more mordern approach

---
+++ assign01/hello.c
@@ -0,0 +1,22 @@
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+
+#define SUCCESS 0
+
+int __init hello_init(void) {
+	pr_info("Hello world !\n");
+
+	return SUCCESS;
+}
+
+void __exit hello_exit(void) {
+	pr_info("Cleaning up module.\n");
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("cjeon@student.42seoul.kr");
+MODULE_DESCRIPTION("simple `hello, world` module.");
